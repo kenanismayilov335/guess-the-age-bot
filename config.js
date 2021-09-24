dc8ed8840fa8ffb947eb45c7f1bddb2127bd4ace
kenanismayilov335 <74872919+kenanismayilov335@users.noreply.github.com>
2021-09-24 20:55:27 +0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = ({
+
+	token: "",	//1188090440:AAGyrziiUOWCsLEwVC24S9dkUEoHJLaa8vw
+
+	botId: 1188090440,
+
+	//1188090440
+
+	rounds: 5,
+
+	waitDelay: 12000,
+
+	timerSteps: 3,
+
+});
